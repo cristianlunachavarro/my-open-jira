bcba4427344a75d5cdce6987208ccfbfaa2e3ff5
Cristian Luna <cristianlunachavarro@gmail.com>
2024-01-12 18:04:34 -0300

    work in progress

--- database/db.ts
+++ database/db.ts
@@ -1,34 +1,34 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connect = async () => {
-    try {
-        const dbConnection = process.env.DB_CONNECTION;
-        if (!dbConnection) {
-            throw new Error('DB connection string is not defined');
-        }
-
-        if (mongoose.connection.readyState !== 1) {
-            await mongoose.connect(dbConnection);
-            console.log('Connected to MongoDB');
-        } else {
-            console.log('Already connected to MongoDB');
-        }
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
+  try {
+    const dbConnection = process.env.MONGODB_URI;
+    if (!dbConnection) {
+      throw new Error("DB connection string is not defined");
     }
+
+    if (mongoose.connection.readyState !== 1) {
+      await mongoose.connect(dbConnection);
+      console.log("Connected to MongoDB");
+    } else {
+      console.log("Already connected to MongoDB");
+    }
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
 };
 
 const disconnect = async () => {
-    try {
-        if (mongoose.connection.readyState !== 0) {
-            await mongoose.disconnect();
-            console.log('Disconnected from MongoDB');
-        } else {
-            console.log('Already disconnected from MongoDB');
-        }
-    } catch (error) {
-        console.error('Error disconnecting from MongoDB:', error);
+  try {
+    if (mongoose.connection.readyState !== 0) {
+      await mongoose.disconnect();
+      console.log("Disconnected from MongoDB");
+    } else {
+      console.log("Already disconnected from MongoDB");
     }
+  } catch (error) {
+    console.error("Error disconnecting from MongoDB:", error);
+  }
 };
 
 export { connect, disconnect };
