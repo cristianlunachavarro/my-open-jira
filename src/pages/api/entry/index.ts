d4125253f5f364990568ffaf404f89b7cde297e0
Cristian Luna <cristianlunachavarro@gmail.com>
2024-01-13 00:07:17 -0300

    set the cors

--- src/pages/api/entry/index.ts
+++ src/pages/api/entry/index.ts
@@ -1,9 +1,8 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import Entry from "../../../../models/entries";
-import { Entry as EntryInterface } from "../../../../interfaces/index";
-
-import cors from "cors";
-import { connect, disconnect } from "../../../../database/db";
+import { NextApiRequest, NextApiResponse } from 'next';
+import cors from 'cors';
+import { connect, disconnect } from '../../../../database/db';
+import Entry from '../../../../models/entries';
+import { Entry as EntryInterface } from '../../../../interfaces/index';
 
 type Data = EntryInterface[] | { error: string };
 
@@ -14,99 +13,96 @@ interface BodyProps {
 }
 
 const corsMiddleware = cors({
-  origin: "https://my-open-jira.vercel.app",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  origin: 'https://my-open-jira.vercel.app',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 });
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
-  next: any
+  res: NextApiResponse<Data>
 ) {
-  await corsMiddleware(req, res, next);
+  corsMiddleware(req, res, async () => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'OPTIONS, POST, GET, PUT, DELETE'
+    );
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, POST, GET, PUT, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    const getEntries = async () => {
+      try {
+        await connect();
+        const entries: EntryInterface[] = await Entry.find();
+        res.status(200).json(entries);
+        await disconnect();
+      } catch (err) {
+        console.error('Error getting entries:', err);
+        const errorResponse: Data = {
+          error: 'Internal Server Error',
+        };
+        res.status(500).json(errorResponse);
+        await disconnect();
+      }
+    };
 
-  const getEntries = async () => {
-    try {
-      await connect();
-      const entries: EntryInterface[] = await Entry.find();
-      res.status(200).json(entries);
-      await disconnect();
-    } catch (err) {
-      console.error("Error creating entry:", err);
-      const errorResponse: Data = {
-        error: "Internal Server Error",
-      };
-      res.status(500).json(errorResponse);
-      await disconnect();
+    const createEntry = async (body: BodyProps) => {
+      const { description, status } = body;
+      try {
+        await connect();
+        const newEntry = await new Entry({
+          description: description,
+          status: status,
+        }).save();
+        res.status(200).json(newEntry);
+        await disconnect();
+      } catch (err) {
+        console.error('Error creating entry:', err);
+        const errorResponse: Data = {
+          error: 'Internal Server Error',
+        };
+        res.status(500).json(errorResponse);
+        await disconnect();
+      }
+    };
+
+    const updateEntry = async (body: BodyProps) => {
+      const { description, status, _id } = body;
+      try {
+        await connect();
+        const updatedEntry = await Entry.findOneAndUpdate(
+          { _id: _id },
+          { description: description, status: status },
+          { new: true }
+        );
+
+        res.status(200).json(updatedEntry);
+        await disconnect();
+      } catch (err) {
+        console.error('Error updating an entry:', err);
+        const errorResponse: Data = {
+          error: 'Internal Server Error',
+        };
+        res.status(500).json(errorResponse);
+        await disconnect();
+      }
+    };
+
+    switch (req.method) {
+      case 'GET':
+        await getEntries();
+        break;
+      case 'POST':
+        await createEntry(req.body);
+        break;
+      case 'PUT':
+        await updateEntry(req.body);
+        break;
+      case 'OPTIONS':
+        res.status(200).end();
+        break;
+      default:
+        res.status(405).json({ error: 'Method Not Allowed' });
     }
-  };
-
-  const createEntry = async (body: BodyProps) => {
-    const { description, status } = body;
-    try {
-      await connect();
-      const newEntry = await new Entry({
-        description: description,
-        status: status,
-      }).save();
-      res.status(200).json(newEntry);
-      await disconnect();
-    } catch (err) {
-      console.error("Error creating entry:", err);
-      const errorResponse: Data = {
-        error: "Internal Server Error",
-      };
-      res.status(500).json(errorResponse);
-      await disconnect();
-    }
-  };
-  const updateEntry = async (body: BodyProps) => {
-    const { description, status, _id } = body;
-    try {
-      await connect();
-
-      const updatedEntry = await Entry.findOneAndUpdate(
-        { _id: _id },
-        { description: description, status: status },
-        { new: true }
-      );
-
-      res.status(200).json(updatedEntry);
-      await disconnect();
-    } catch (err) {
-      console.error("Error updating an entry:", err);
-      const errorResponse: Data = {
-        error: "Internal Server Error",
-      };
-      res.status(500).json(errorResponse);
-      await disconnect();
-    }
-  };
-
-  const { body, method } = req;
-
-  switch (method) {
-    case "GET":
-      await getEntries();
-      break;
-    case "POST":
-      await createEntry(body);
-      break;
-    case "PUT":
-      await updateEntry(body);
-      break;
-    case "OPTIONS":
-      res.status(200).end();
-      break;
-    default:
-      res.status(405).json({ error: "Method Not Allowed" });
-  }
+  });
 }
